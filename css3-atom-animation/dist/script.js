3797f91401f81eb385b80e05b569ca273d5ac7bb
purneshyogi <purneshyogi@gmail.com>
2021-10-01 23:04:48 +0530

    shows current datetime

--- css3-atom-animation/dist/script.js
+++ css3-atom-animation/dist/script.js
@@ -1,1 +1,11 @@
 //Dont have time to refactor right now
+let Dtime = document.getElementById("distime")
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+Dtime.textContent = dateTime
+
+
